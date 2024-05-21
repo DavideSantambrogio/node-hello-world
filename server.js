40b8a7b351b70e743743832dac9b5e1707354823
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2024-05-21 15:18:29 +0200

    add: message from env file

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const http = require("http");
 // Configurazione delle variabili di ambiente
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+require('dotenv').config();
 
 http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("Hello World!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<h1>${process.env.MY_VARIABLE}</h1>`);
 }).listen(port, host, () => {
-  const serverUrl = `http://${host}:${port}`;
-  console.log(`Server avviato su ${serverUrl}`);
+    const serverUrl = `http://${host}:${port}`;
+    console.log(`Server avviato su ${serverUrl}`);
 });
